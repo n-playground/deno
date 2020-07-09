2ce53caee7bf56eb1863cba0a547d8ab11d6ccc1
norictech <ahmad.uji1902@gmail.com>
2020-07-09 21:47:54 +0700

    variable with type

---
+++ src-playground/hello.ts
@@ -0,0 +1,7 @@
+// const greeting = 'Hello World!' // this is ok
+
+const greeting: string = 'Hello World!' // this is ok
+
+// const greeting: string = 1 // this is not ok, Type '`' is not assignable to type 'string'.
+
+console.log(greeting)
